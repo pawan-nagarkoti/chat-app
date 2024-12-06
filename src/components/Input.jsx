9f0a681bf886ddd0f7cdaa58ad816b9862303cd2
Pawan <pawan@Pawans-MacBook-Air.local>
2024-12-06 22:47:06 +0530

    Integrate register page api and setup env files

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useId, forwardRef } from "react";
 
-export default function Input({ label = "", type = "", placeholder = "" }) {
+const Input = forwardRef(({ label = "", type = "", ...props }, ref) => {
+  const id = useId();
+
   return (
     <>
-      <label htmlFor="username" className="block text-sm font-medium text-gray-600 mb-1">
+      <label htmlFor={id} className="block text-sm font-medium text-gray-600 mb-1">
         {label}
       </label>
       <input
         type={type}
-        id="username"
-        placeholder={placeholder}
+        id={id}
+        ref={ref} // Forward the ref to the input element
+        {...props}
         className="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring focus:ring-blue-300"
       />
     </>
   );
-}
+});
+
+export default Input;
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,29 +1,49 @@
 import { Link } from "react-router-dom";
 import { Input } from "../components";
+import { useState } from "react";
+import { registerUser } from "@/api";
 
 export default function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegisterData = (e) => {
+    e.preventDefault();
+    // create a object and send to the register payload api
+    const registerUserDetail = {
+      email,
+      username,
+      password,
+      role: "ADMIN",
+    };
+    registerUser(registerUserDetail);
+  };
   return (
     <>
       <div className="min-h-screen bg-gray-100 flex items-center justify-center">
         <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm">
           <h2 className="text-2xl font-bold text-center text-gray-700 mb-6">Register</h2>
-          <form>
+          <form onSubmit={handleRegisterData}>
             {/* Email field */}
             <div className="mb-4">
-              <Input label="email" type="email" placeholder="Enter your email" />
+              <Input label="email" type="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} />
             </div>
             {/* Username Field */}
             <div className="mb-4">
-              <Input label="Username" type="text" placeholder="Enter your username" />
+              <Input label="Username" type="text" placeholder="Enter your username" onChange={(e) => setUsername(e.target.value)} />
             </div>
 
             {/* Password Field */}
             <div className="mb-4">
-              <Input label="Password" type="password" placeholder="Enter your password" />
+              <Input label="Password" type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} />
             </div>
 
             {/* Submit Button */}
-            <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+            >
               Login
             </button>
           </form>
